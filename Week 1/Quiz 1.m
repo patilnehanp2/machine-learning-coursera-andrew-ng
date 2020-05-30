a0b1716a1e780e8d2558e80f718e1c3c702915b9
patilnehanp2 <61180949+patilnehanp2@users.noreply.github.com>
2020-05-30 14:58:36 +0530

    Create Quiz 1.m

---
+++ Week 1/Quiz 1.m
@@ -0,0 +1,60 @@
+1.Question 1
+A computer program is said to learn from experience E with respect to some task T and some performance measure P if its performance on 
+T, as measured by P, improves with experience E.Suppose we feed a learning algorithm a lot of historical weather data, and have it 
+learn to predict weather. What would be a reasonable choice for P?
+
+(a) The process of the algorithm examining a large amount of historical weather data.
+(b) The probability of it correctly predicting a future date's weather.
+(c) None of these.
+(d) The weather prediction task.
+
+Ans :- The probability of it correctly predicting a future date's weather.
+
+2.Question 2
+Suppose you are working on weather prediction, and your weather station makes one of three predictions for each day's weather: Sunny, 
+Cloudy or Rainy. You'd like to use a learning algorithm to predict tomorrow's weather. Would you treat this as a classification or 
+a regression problem?
+
+(a) Regression
+(b) Classification
+
+Ans :- Classification
+
+3.Question 3
+Suppose you are working on stock market prediction. You would like to predict whether or not a certain company will declare bankruptcy
+within the next 7 days (by training on data of similar companies that had previously been at risk of bankruptcy). Would you treat this
+as a classification or a regression problem?
+
+(a) Regression
+(b) Classification
+
+Ans :- Classification
+
+4.Question 4
+Some of the problems below are best addressed using a supervised learning algorithm, and the others with an unsupervised learning 
+algorithm. Which of the following would you apply supervised learning to? (Select all that apply.) In each case, assume some 
+appropriate dataset is available for your algorithm to learn from.
+
+(a) In farming, given data on crop yields over the last 50 years, learn to predict next year's crop yields.
+(b) Examine a web page, and classify whether the content on the web page should be considered "child friendly" 
+    (e.g., non-pornographic, etc.) or "adult."
+(c) Given data on how 1000 medical patients respond to an experimental drug (such as effectiveness of the treatment, side effects, etc.),
+    discover whether there are different categories or "types" of patients in terms of how they respond to the drug, and if so what 
+    these categories are.
+(d) Given a large dataset of medical records from patients suffering from heart disease, try to learn whether there might be different 
+    clusters of such patients for which we might tailor separate treatments.
+
+Ans :- 
+       (a) In farming, given data on crop yields over the last 50 years, learn to predict next year's crop yields.
+       (b) Examine a web page, and classify whether the content on the web page should be considered "child friendly" 
+           (e.g., non-pornographic, etc.) or "adult."
+           
+5.Question 5
+Which of these is a reasonable definition of machine learning?
+
+(a) Machine learning is the science of programming computers.
+(b) Machine learning learns from labeled data.
+(c) Machine learning is the field of allowing robots to act intelligently.
+(d) Machine learning is the field of study that gives computers the ability to learn without being explicitly programmed.
+
+Ans :- Machine learning is the field of study that gives computers the ability to learn without being explicitly programmed.
